71e4cbab36fcc158ada418e1907dea01f98e9d3a
James Reddington <reddingtonjamesr@gmail.com>
2022-02-26 19:49:29 +0000

    Genetic Algorithm

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def initial_population():
             ran = randint(0,1)
             sentence = sentence + str(ran)
         population.append(sentence)
-    print(population)
+    print("\nInitial Population:\n",population)
     return population
 
 # Evaluate fitness
@@ -23,12 +23,15 @@ def eval_fitness(pop):
         list.append(fit)
     return list
 
+# sort population
 def sort_population(i):
     return i[1]
 
+# select best fit
 def select_best(pop,fit):
     list = []
     output = []
+    top = 3
     for n in range(0,len(pop)):
         list.append([pop[n],fit[n]])
 
@@ -36,12 +39,23 @@ def select_best(pop,fit):
 
     for k in list:
         output.append(k[0])
-    print(output[0:3])
-
+    print("Top", top,"\n",output[0:top])
     return output[0:3]
 
+# Crossover Method
+def crossover(s1,s2, point):
+    print("\nCrossover Opperations:\nParents")
+    c1 = s1[0:point] + s2[point:len(s1)]
+    c2 = s2[0:point] + s1[point:len(s1)]
+    print(s1, s2)
+    print("Children")
+    print(c1,c2)
+    return
+
+# Methods used by Algorithm
 population = initial_population()
 fit = eval_fitness(population)
-select_best(population,fit)
+best = select_best(population,fit)
+cross = crossover(best[0],best[1],randint(0,len(best[0])))
 
 
